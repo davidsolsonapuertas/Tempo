9181b541f52b775cca362338e6d4167e5e6d7765
Zhiqin He <32912199+Badly-Timed-Joke@users.noreply.github.com>
2022-04-30 18:33:18 -0400

    Create playlist algorithm complete
    Underlying algorithm for creating a playlist is finished. Endpoint function is still incomplete.

--- tempo_backend/src/app.py
+++ tempo_backend/src/app.py
@@ -5,7 +5,7 @@ import datetime
 from db import db
 from flask import Flask, request
 
-db_filename = "auth.db"
+db_filename = "tempo.db"
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
@@ -51,40 +51,108 @@ def extract_token(request):
 # ------------- ROUTES -------------
 
 @app.route("/")
-
 @app.route("/tempo/playlist/", methods=["POST"])
 def create_new_playlist():
     """
-    Endpoint for creating a new playlist from Spotify with specified length\n
-    Request body:\n
-    \t- hours: Hours component of playlist length
-    \t- minutes: Minutes component of playlist length \n
-    Returns:\n 
-    \t- TODO: ADD RETURNED DATA HERE
+    Endpoint for creating a new playlist from Spotify with specified length
+
+    Returns:
+        list: List of tracks with total playtime of specified length
     """
+
     # verify user
-    
+
     was_successful, session_token = extract_token(request)
-    
+
     if not was_successful:
         return session_token
-    
+
     user = users_dao.get_user_by_session_token(session_token)
-    if not user or not user.verify_session_token(session_token):
+    # TODO: if statment to check for invalid session tokens
+    if not user or True:
         return failure_response("Invalid session token")
-    
+
     # create playlist
-        
+
     body = json.loads(request.data)
     hours = body.get("hours")
     minutes = body.get("minutes")
-    
+
     if hours is None or minutes is None:
         return failure_response("Request body is missing hours or minutes")
-    
+
+    # * pseudocode for creating a playlist w/ given length
+    """
+    int length = int(hours)*60 + int(minutes)
+    get recommended tracks request: 
+        # of tracks = 100
+        max length of track = length
+    recommended = list of recommended tracks from Spotify
+    playlist = findTracklistSum(recommended, length)
+    return playlist as json list of tracks (or whatever FE needs to get the song from Spotify)
+    """
+
     pass
 
-# ------------- RUN APP -------------
 
+def find_tracklist_sum(tracklist, sum, increment_fuzzy=60):
+    """    
+    Find a sublist of tracklist with total playtime equal to given value sum
+
+    Args:
+        tracklist (list): List of tracks
+        sum (int): Target sum to find in seconds
+        increment_fuzzy (int, optional): Number of seconds to increment fuzzy by. Defaults to 60.
+
+    Returns:
+        list: List of tracks with total playtime equal to given value sum
+    """
+    playlist = []
+    fuzzy = 0
+    while len(playlist) == 0:
+        playlist = find_tracklist_sum_helper(
+            tracklist, len(tracklist), sum, fuzzy)
+        increment_fuzzy += 60
+
+    return playlist
+
+
+def find_tracklist_sum_helper(tracks, n, sum, fuzzy):
+    """
+    Helper function for finding tracklist with total playtime equal to given value sum
+
+    Returns empty list if one is not found
+
+    Args:
+        tracks (list): List where tracks to be searched through
+        n (int): Length of tracks
+        sum (int): Target sum to find in seconds
+        fuzzy (int): Leniency on how close the tracklist playtime and sum should be in seconds
+
+    Returns:
+        list: List of tracks with total playtime equal to given value sum or empty list if one is not found
+    """
+    if n == 0:
+        return []
+
+    last_track = tracks[n-1]
+    last_length = last_track["duration_ms"]/1000
+
+    if abs(sum - last_length) <= fuzzy:
+        return [last_track]
+
+    keep_last = find_tracklist_sum_helper(tracks, n-1, sum-last_length, fuzzy)
+    skip_last = find_tracklist_sum_helper(tracks, n-1, sum, fuzzy)
+
+    if not len(keep_last) == 0:
+        keep_last += [last_track]
+
+    if len(keep_last) == 0:
+        return skip_last
+    else:
+        return keep_last
+
+
+# ------------- RUN APP -------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
